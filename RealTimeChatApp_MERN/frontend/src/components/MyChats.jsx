cac488edebbfe1134eaa7c6bcb579a403093e678
harshita <harshita@logicrays.com>
2024-10-04 17:35:34 +0530

    03

--- RealTimeChatApp_MERN/frontend/src/components/MyChats.jsx
+++ RealTimeChatApp_MERN/frontend/src/components/MyChats.jsx
@@ -3,31 +3,86 @@ import React, { useState } from 'react'
 import { chatState } from '../ChatProvider'
 import ChatLoading from './ChatLoading'
 
-const MyChats = () => {
+const MyChats = ({ fetchChat }) => {
     const {selectedChat,setSelectedChat,user,chats,setChats} = chatState()
     const [loggedInUser,setLoggedInUser] = useState()
   return (
     <div>
-      <Box display={{base:selectedChat ? 'none':'flex',md:'flex'}} p={2} flexDir="column"
-      alignItems="center" bg="white" w={{base:'100%',md:'30%'}} borderWidth={1}>
-        <Box pb={3} px={3} fontSize={{base:'30px',md:'20px'}} display="flex" w="100%"
-        justifyContent="space-between" alignItems="center">
-            My Chats</Box>
-            <Box display="flex" flexDir="column" p={3}
-            w="100%" h="100%" borderRadius="lg" overflowY="hidden">  
-                {chats.length!=0 ? 
-                    <Stack overflowY="scroll">
-                        {chats.map((chat,i)=>
-                            <Box bg="lightskyblue">
-                                <Text>user</Text>
-                            </Box>
-                        )}
-                    </Stack>
-                :<ChatLoading/>
-                }
-            
-        </Box>
+      <Box
+      d={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      flexDir="column"
+      alignItems="center"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "31%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <Box
+        pb={3}
+        px={3}
+        fontSize={{ base: "28px", md: "30px" }}
+        fontFamily="Work sans"
+        display="flex"
+        w="100%"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        My Chats
+        <GroupChatModal>
+          <Button
+            display="flex"
+            fontSize={{ base: "17px", md: "10px", lg: "17px" }}
+            rightIcon={<AddIcon />}
+          >
+            New Group Chat
+          </Button>
+        </GroupChatModal>
       </Box>
+      <Box
+        display="flex"
+        flexDir="column"
+        p={3}
+        bg="#F8F8F8"
+        w="100%"
+        h="100%"
+        borderRadius="lg"
+        overflowY="hidden"
+      >
+        {chats ? (
+          <Stack overflowY="scroll">
+            {chats.map((chat) => (
+              <Box
+                onClick={() => setSelectedChat(chat)}
+                cursor="pointer"
+                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+                color={selectedChat === chat ? "white" : "black"}
+                px={3}
+                py={2}
+                borderRadius="lg"
+                key={chat._id}
+              >
+                <Text>
+                  {!chat.isGroupChat
+                    ? getSender(loggedUser, chat.users)
+                    : chat.chatName}
+                </Text>
+                {chat.latestMessage && (
+                  <Text fontSize="xs">
+                    <b>{chat.latestMessage.sender.name} : </b>
+                    {chat.latestMessage.content.length > 50
+                      ? chat.latestMessage.content.substring(0, 51) + "..."
+                      : chat.latestMessage.content}
+                  </Text>
+                )}
+              </Box>
+            ))}
+          </Stack>
+        ) : (
+          <ChatLoading />
+        )}
+      </Box>
+    </Box>
     </div>
   )
 }
